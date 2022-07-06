9210baf425caa1758aba2b72261fd945df9c10c1
rover0811 <rover0811@hotmail.com>
2022-07-06 18:24:18 +0900

    Add Python Workout (7/6)

--- 2675.py
+++ 2675.py
@@ -1,13 +1,29 @@
-testingNum=int(input())
-for i in range(testingNum):
-    repeatNum,string=map(str,input().split())
-    for j in string:
-        for k in range(int(repeatNum)):
-            print(j,end='')
-    print()
+# testingNum=int(input())
+# for i in range(testingNum):
+#     repeatNum,string=map(str,input().split())
+#     for j in string:
+#         for k in range(int(repeatNum)):
+#             print(j,end='')
+#     print()
 
+# a=(10,20)
+# a=(30,40)
+# print(type(a))
 
-sorted(repeatNum,reverse=True)
+# c,d=10,20
+# c=15
+# print(type(c))
+
+# def test():
+#     return 1,3
+
+# a,b=test()
+
+a=list(map(int,input().split(" ")))
+
+print(a)
+for i in a:
+    print(i)
 
 
 
