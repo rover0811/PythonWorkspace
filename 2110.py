cdff20cedff472ad34a3fc63d128b1671834ff90
DESKTOP-1BDSLRQ\김종원 <rover0811@hotmail.com>
2022-07-09 11:11:29 +0900

    Add Python Workouts (7/9)

---
+++ 2110.py
@@ -0,0 +1,6 @@
+N,C= map(int, input().split())
+sharearray=[]
+for i in range(N):
+    sharearray.append(int(input()))
+
+print("hi") #0100100100 11100111011-> print가 저장된 파일과 니모닉화한 파일과 연결을 해줌 링커->    .obj->.exe 실행파일이 만들어집니다.
