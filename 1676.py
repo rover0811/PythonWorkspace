2588aae797d1b56ab072fd0f71ad71b290301fb0
rover0811 <rover0811@hotmail.com>
2023-04-04 21:35:22 +0900

    [Add] 1676 시도중

--- 1676.py
+++ 1676.py
@@ -1,2 +1,15 @@
+import sys
 import math
-print([i if i==0 else None for i in str(math.factorial(int(input())))[::-1]])
+input = sys.stdin.readline
+
+N = int(input()) #입력 받은 수
+
+for i in range(N,0,-1):
+    print(i,math.factorial(i),str(math.factorial(i)).count("0"))
+
+# count = 0
+# while N >= 5: 
+#     count += N//5 
+#     N //= 5 
+    
+# print(count)
