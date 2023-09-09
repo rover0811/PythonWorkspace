8e27532f0e45b30b84e6b49cb4e17e1cc531104a
HyunSoo <84193252+rover0811@users.noreply.github.com>
2023-09-09 14:42:28 +0000

    [Fix] n-queen 문제

--- 9663.py
+++ 9663.py
@@ -22,8 +22,6 @@ def dfs(y, chess_board):
 
 def set_queen(x,y,t_chess_board):
 
-    # print("x,y: ",x,y)
-    # print("t_chess_board: ",t_chess_board)
     # chess_board를 깊은 복사해서 사용해야 한다.
     l_chess_board = [i[:] for i in t_chess_board]
     l_chess_board[x][y] = 1 # 퀸 배치
@@ -42,7 +40,6 @@ def set_queen(x,y,t_chess_board):
         if temp_x >= n or temp_y >= n:
             break
         l_chess_board[temp_x][temp_y] = -1
-        # print("x,y turn -1: ",temp_x,temp_y)
     
     # x,y 이후 우상향하는 대각선을 모두 -1 처리
     temp_x = x
@@ -54,21 +51,6 @@ def set_queen(x,y,t_chess_board):
         if temp_x < 0 or temp_y >= n:
             break
         l_chess_board[temp_x][temp_y] = -1
-        # print("x,y turn -1: ",temp_x,temp_y)
-
-
-    # for row in range(x+1,n):
-    #     for col in range(y+1,n):
-    #         l_chess_board[row][col] = -1
-    #         print("x,y turn -1: ",row,col)
-
-    
-    # # x,y 이후 우상향하는 를 모두 -1 처리
-    # for row in range(x-1,-1,-1): # x가 0일 때까지
-    #     for col in range(y+1,n): # y가 n-1일 때까지
-    #         l_chess_board[row][col] = -1
-    
-    # print("l_chess_board: ",l_chess_board)
 
     return l_chess_board
 
